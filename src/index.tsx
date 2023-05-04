40fba1aacdea97d9f54676dbf0d034af7fb9f2f7
Omar Baabouj <baabouj.dev@gmail.com>
2023-05-04 12:01:56 +0100

    feat(src/index.tsx): switch to the React 18 API (use createRoot instead of ReactDOM.render)

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import { createRoot } from "react-dom/client";
 
-ReactDOM.render(
+import App from "./App";
+import "./index.css";
+
+const container = document.getElementById("root");
+const root = createRoot(container!);
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
