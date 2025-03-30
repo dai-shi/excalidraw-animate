94f352a0f0571eb35c3c17fb6e4c61ee53a4e416
Daishi Kato <dai-shi@users.noreply.github.com>
2025-03-30 09:51:45 +0900

    Migrate to pnpm and update actions (#61)
    * migrate to pnpm
    
    * update actions
    
    * hack type

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
 
+// @ts-expect-error no types
 test("renders without errros", () => {
   render(<App />);
 });
