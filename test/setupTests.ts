fcf9e1b6111f6d6af80224c812ef3a624b473922
Steven Semyung Oh <semyung.dev@gmail.com>
2025-04-01 16:33:11 +0900

    feat: migrate from jest to vitest

--- test/setupTests.ts
+++ test/setupTests.ts
@@ -3,7 +3,7 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom/extend-expect";
-import "jest-canvas-mock";
+import "vitest-canvas-mock";
 import crypto from "crypto";
 
 const element = document.createElement("div");
