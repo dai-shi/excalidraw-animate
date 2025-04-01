a3ac23be33082974949f2196d513611ed31f6e17
steven semyung oh <semyung.dev@gmail.com>
2025-04-01 21:15:04 +0900

    chore: remove crypto stub

--- test/setupTests.ts
+++ test/setupTests.ts
@@ -4,14 +4,7 @@
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom/extend-expect";
 import "vitest-canvas-mock";
-import crypto from "crypto";
 
 const element = document.createElement("div");
 element.id = "root";
 document.body.appendChild(element);
-
-(window as any).crypto = {
-  getRandomValues: function (buffer: any) {
-    return crypto.randomFillSync(buffer);
-  },
-};
