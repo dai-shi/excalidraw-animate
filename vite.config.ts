4b371c904aaf53b9fd6125df9dbc4f77a8d7c324
steven semyung oh <90446271+nemyung@users.noreply.github.com>
2025-04-02 22:12:06 +0900

    fix: add base url to deploy gh-page (#67)

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import { configDefaults } from "vitest/config";
 
 export default defineConfig({
+  base: "/excalidraw-animate/",
   plugins: [react()],
   build: {
     outDir: "build",
