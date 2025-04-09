13a3669c1ca75c9a362c8505d1f391a7c881b977
kei95 <kei0613shine.star@gmail.com>
2025-04-10 00:21:18 +0900

    update: Update config not to import json in open-color in test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,6 @@
-import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
 
-// @ts-expect-error no types
 test("renders without errros", () => {
   render(<App />);
 });
