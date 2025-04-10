600b90b55180c1db912a0f0844c7b1084f330b42
daishi <daishi@axlight.com>
2025-04-10 23:41:33 +0900

    eliminate import React

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "./App";
@@ -7,7 +7,7 @@ import "./index.css";
 const container = document.getElementById("root");
 const root = createRoot(container!);
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
