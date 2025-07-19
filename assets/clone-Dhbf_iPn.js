fc04c8d4bcf199c253a0bbb3c2e7505b881ee95d
dai-shi <dai-shi@users.noreply.github.com>
2025-07-19 06:47:40 +0000

    deploy: 2bdd5003d8f6c88262a19a479bd4e7a93d48e083

--- assets/clone-Dhbf_iPn.js
+++
@@ -1,1 +0,0 @@
-import{b as r}from"./graph-BYi4p_Hu.js";var e=4;function a(o){return r(o,e)}export{a as c};
---
+++ assets/subset-worker.chunk-DM-K4Npi.js
@@ -0,0 +1,1 @@
+import{Commands as e,subsetToBinary as t}from"./subset-shared.chunk-DL2G5_Wc.js";import"./index-BxzW2xWs.js";var m=import.meta.url?new URL(import.meta.url):void 0;typeof window>"u"&&typeof self<"u"&&(self.onmessage=async a=>{switch(a.data.command){case e.Subset:let s=await t(a.data.arrayBuffer,a.data.codePoints);self.postMessage(s,{transfer:[s]});break}});export{m as WorkerUrl};
