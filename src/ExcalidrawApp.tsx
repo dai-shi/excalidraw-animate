7fd8ed6c4b5a4531a926ff2ddb8834215c8751e9
daishi <daishi@axlight.com>
2025-07-19 14:16:35 +0900

    animate panel

--- src/ExcalidrawApp.tsx
+++ src/ExcalidrawApp.tsx
@@ -1,9 +1,15 @@
-import { Excalidraw } from '@excalidraw/excalidraw';
+import { useState } from 'react';
+import { Excalidraw, Footer, Sidebar } from '@excalidraw/excalidraw';
 import type { AppState, BinaryFiles } from '@excalidraw/excalidraw/types';
 import type { ExcalidrawElement } from '@excalidraw/excalidraw/element/types';
 
 // eslint-disable-next-line import/no-unresolved
 import '@excalidraw/excalidraw/index.css';
+
+const AnimatePanel = () => {
+  // TODO
+  return <p>TODO</p>;
+};
 
 const ExcalidrawApp = ({
   initialData,
@@ -18,6 +24,7 @@ const ExcalidrawApp = ({
     files: BinaryFiles;
   }) => void;
 }) => {
+  const [docked, setDocked] = useState(false);
   return (
     <div style={{ height: '100vh', width: '100vw' }}>
       <Excalidraw
@@ -25,7 +32,22 @@ const ExcalidrawApp = ({
         onChange={(elements, appState, files) =>
           onChangeData({ elements, appState, files })
         }
-      />
+      >
+        <Sidebar name="custom" docked={docked} onDock={setDocked}>
+          <Sidebar.Header />
+          <AnimatePanel />
+        </Sidebar>
+        <Footer>
+          <Sidebar.Trigger
+            name="custom"
+            style={{
+              marginLeft: '0.5rem',
+            }}
+          >
+            Toggle Animate Panel
+          </Sidebar.Trigger>
+        </Footer>
+      </Excalidraw>
     </div>
   );
 };
