c7653860f40d56565246e893f4ea1fc638fd10eb
dai-shi <dai-shi@users.noreply.github.com>
2026-01-22 06:45:31 +0000

    deploy: a64dabe1f0887bd7923fd478f8265fe956a59afe

--- assets/clone-BkQZFrKW.js
+++
@@ -1,1 +0,0 @@
-import{b as r}from"./graph-BD3cftQR.js";var e=4;function a(o){return r(o,e)}export{a as c};
---
+++ assets/subset-worker.chunk-XfEahOq2.js
@@ -0,0 +1,1 @@
+import{Commands as e,subsetToBinary as t}from"./subset-shared.chunk-BJsRW_uv.js";import"./index-DklcEss_.js";var m=import.meta.url?new URL(import.meta.url):void 0;typeof window>"u"&&typeof self<"u"&&(self.onmessage=async a=>{switch(a.data.command){case e.Subset:let s=await t(a.data.arrayBuffer,a.data.codePoints);self.postMessage(s,{transfer:[s]});break}});export{m as WorkerUrl};
